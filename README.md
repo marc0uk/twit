0585fc2eb13782f513dd7723df6697f3e606d73b
Marco Riello <riellomarco@gmail.com>
2016-01-31 22:27:43 +0000

    Initial import of untested script

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # twit
-My vey silly twitter script
+My vey silly twitter script. The purpose is to use the script in a docker container used to post tweets (status updates) to a bot account from scripts. How silly is that ?
---
+++ twit.py
@@ -0,0 +1,36 @@
+import sys, os
+import tweepy
+
+# File with colon-separaten consumer/access token and secret
+consumer_file='twitter.consumer'
+access_file='twitter.access'
+
+def __load_auth(file):
+    if os.path.exists(file):
+        with open(file) as f:
+            tokens = f.readline().split(':')
+        if len(tokens) == 2:
+            return tokens[0],tokens[1]
+        else:
+            raise ValueError("Expecting two colon-separated tokens")
+    else:
+        raise IOError("File not found: %s" % file)
+
+def twit(message, secret_dir='/secret'):
+    #
+    # Load the twitter consumer and access tokens and secrets
+    consumer_token, consumer_secret = __load_auth(os.path.join(secret_dir, consumer_file))
+    access_token, access_secret = __load_auth(os.path.join(secret_dir, access_file))
+    #
+    # Perform OAuth authentication
+    auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    #
+    # Create the API and post the status update
+    api = tweepy.API(auth)
+    api.update_status(message)
+
+if __name__ == '__main__':
+    tokens = sys.argv[1:]
+    #
+    twit(' '.join(tokens))
